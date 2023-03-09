2535e4697b067580b4b614150fd04fee5ef65c1f
Dagore <david.gomez.redondo.dev@gmail.com>
2023-03-09 16:28:00 +0100

    backend added

--- frontend/src/views/Main.tsx
+++ frontend/src/views/Main.tsx
@@ -1,11 +1,21 @@
 import { IPizza } from '../interfaces/PizzaInterface'
 import Pizza from '../components/Pizza'
+import { useEffect, useState } from 'react'
 
 export default function Main (): JSX.Element {
-  const pizzas: IPizza[] = [
-    { id: 1, name: 'Cheese pizza', description: 'very cheesy' },
-    { id: 2, name: 'Al Tono pizza', description: 'lots of tuna' }
-  ]
+  const [pizzas, setPizzas] = useState<IPizza[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:5059/pizza')
+      .then(response => {
+        response.json()
+          .then(data => {
+            setPizzas(data)
+          })
+          .catch(error => console.log(error))
+      })
+      .catch(error => console.log('FETCH', error))
+  }, [])
 
   return (
     <main>
